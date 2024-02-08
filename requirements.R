106d6fb75d3f4526354fc0644fa8d36470f7185a
Philippe Cote <pcote@ualberta.ca>
2024-02-08 11:12:28 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,1 +1,5 @@
-install.packages(c("tidyquant","plotly","TTR"), dependencies = TRUE)
+p <- c("tidyquant","plotly","TTR","tidyverse")
+new.packages <- p[!(p %in% installed.packages()[, "Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages, dependencies = TRUE)
+}
